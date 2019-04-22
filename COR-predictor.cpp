9438e3402c5c9fd4b4ec1527bd09c003d9d01c50
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-04-22 17:45:09 -0500

    Update COR-predictor.cpp

--- COR-predictor.cpp
+++ COR-predictor.cpp
@@ -1,3 +1,25 @@
+/*
+ * COR-predictor.cpp
+ *
+ *  Copyright 2019
+ *      J. Ball (SchroedingersFerret)
+ */
+ 
+//This file is part of COR-Predictor-CUDA.
+//
+//   COR-Predictor-CUDA is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   COR-Predictor is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with COR-Predictor-CUDA.  If not, see <https://www.gnu.org/licenses/>.
+
 template <typename T>
 KernelArray<T> convertToKernel(thrust::device_vector<T> &dVec, int i, int j, int k)
 {
