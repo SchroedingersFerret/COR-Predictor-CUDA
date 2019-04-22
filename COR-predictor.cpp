83d6549d7385afb689e336b9094457ce35f68a1b
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-04-22 13:40:48 -0500

    Update COR-predictor.cpp

--- COR-predictor.cpp
+++ COR-predictor.cpp
@@ -469,3 +469,225 @@ bool COR_predictor::Use_random()
 	}
 	return random;
 }
+
+//prints the parameters in the terminal
+void COR_predictor::Print_parameters(std::vector<std::vector<float> > param)
+{
+	int ni = param.size();
+	int nj = param[0].size();
+	for (int i=0; i<ni; ++i)
+	{
+		for (int j=0; j<nj; ++j)
+		{
+			std::cout << param[i][j] << ",";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+}
+
+//sets bool write
+bool COR_predictor::Set_write(char input)
+{
+	switch(input)
+	{
+		case 'y':
+		case 'Y':	return true;
+
+		case 'n':
+		case 'N':	return false;
+
+		default	:	throw "\nInvalid input\nEnter y/n: ";
+	}
+}
+	
+//runs Set_write() and writes parameters to file depending on user input
+void COR_predictor::Write_parameters(std::vector<std::vector<float> > param)
+{
+	bool write = false;
+	std::cout << "Write these parameters to 'cor_parameters.csv'?\nPrevious values will be overwritten.\nEnter y/n: ";
+	char input;
+	
+	while(std::cin.get(input))
+	{
+		char dummy[30];
+		std::cin.getline(dummy,30);
+		std::cout << "\n";
+		try
+		{
+			write = Set_write(input);
+		}
+		catch(const char * s)
+		{
+			std::cout << s;
+			continue;
+		}
+		break;
+	}
+	
+	if (write)
+	{
+		write_csv2d(param,"cor_parameters.csv");
+		std::cout << "Parameters written.\n\n";
+	}
+	else
+		std::cout << "Optimization terminated without writing new parameters.\n\n";
+}
+
+//shows time taken for optimization
+void COR_predictor::Show_time(int time)
+{
+	int hours, minutes, seconds;
+	seconds = time%60;
+	hours = time/3600;
+	minutes = time/60-hours*60;
+	std::cout << "Execution time: " << hours << ":";
+	if (minutes<10)
+		std::cout << 0;
+	std::cout << minutes << ":";
+	if (seconds<10)
+		std::cout << 0;
+	std::cout << seconds << "\n\n";
+}
+
+//optimizes parameters to training data
+void COR_predictor::Optimize()
+{
+	random_parameters = Use_random();
+	clock_t tStart = clock();
+	genetic::run();
+	std::cout << "\n\nParameters found:\n\n" ;
+	Print_parameters(parameters_global);
+	Show_time((clock()-tStart)/CLOCKS_PER_SEC);
+	Write_parameters(parameters_global);
+	quit_cor = Return_quit();
+}
+
+//gets independent variables for prediction
+std::vector<float> COR_predictor::pGet_independent()
+{
+	std::vector<float> x(nx);
+	float input;
+	std::cout << "Enter the yield strength of the first object in GPa.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[0] = input;
+	
+	std::cout << "Enter the yield strength of the second object in GPa.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[1] = input;
+	
+	std::cout << "Enter the Young's modulus of the first object in GPa.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[2] = input;
+	
+	std::cout << "Enter the Young's modulus of the second object in GPa.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[3] = input;
+	
+	std::cout << "Enter the density of the first object.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[4] = input;
+	
+	std::cout << "Enter the density of the second object.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[5] = input;
+	
+	std::cout << "Enter the objects' relative velocity in m/s.\n";
+	std::cin >> input;
+	std::cin.get();
+	std::cout << "\n";
+	x[6] = input;
+	
+	return x;
+}	
+
+//predicts a coefficient of restitution
+void COR_predictor::Predict()
+{
+	thrust::host_vector<float> x(n_data*nx);
+	flatten2dToHost(x,independent);
+	thrust::host_vector<float> param(parameters_global.size()*parameters_global[0].size());
+	flatten2dToHost(param,parameters_global);
+	float pred_y = optimization::f(convertToKernel(x,n_data,nx,1),0,
+		convertToKernel(param,parameters_global.size(),parameters_global[0].size(),1),0);
+	std::cout.precision(3);
+	std::cout.setf(std::ios::fixed, std::ios::floatfield);
+	std::cout << "e = " << pred_y << "\n\n";
+	quit_cor = Return_quit();
+}	
+
+//shows the main menu
+void COR_predictor::Show_main_menu()
+{
+	std::cout << "Enter '1' to enter a new training datapoint.\n\n";
+	std::cout << "Enter '2' to optimize the parameters.\n\n";
+	std::cout << "Enter '3' to predict a coefficient of restitution.\n\n";
+	std::cout << "Enter '4' to quit.\n\n";
+}
+
+//sets bool quit
+int COR_predictor::Set_mode(char input)
+{
+	switch(input)
+	{
+		case '1' :  return 1;
+
+		case '2' : 	return 2;
+
+		case '3' : 	return 3;
+
+		case '4' : 	return 4;
+
+		default : 	throw "Invalid input. Please enter '1','2','3', or '4'.\n\n";
+	}
+}	
+
+//user can select mode of operation from the main menu
+void COR_predictor::Main_menu()
+{
+	Show_main_menu();
+	int mode;
+	char input;
+	while(std::cin.get(input))
+	{
+		char dummy[30];
+		std::cin.getline(dummy,30);
+		std::cout << "\n";
+		try
+		{
+			mode = Set_mode(input);
+		}
+		catch(const char * s)
+		{
+			std::cout << s;
+			continue;
+		}
+		break;
+	}
+		
+	switch(mode)
+	{
+		case 1 	:  	Enter();
+					break;
+		case 2  :	Optimize();
+					break;
+		case 3  :  	Predict();
+					break;
+		case 4  :  	quit_cor = true;
+					break;	
+		default : 	break;
+	}
+}
+
