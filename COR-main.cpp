f791e055db267d5befe37e49e57a9efaa5fdcdb0
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-04-22 17:38:06 -0500

    Update and rename main.cpp to COR-main.cpp

---
+++ COR-main.cpp
@@ -0,0 +1,40 @@
+/*
+ * COR-main.cpp
+ *
+ *  Copyright 2019
+ *      J. Ball (SchroedingersFerret)
+ */
+
+//This file is part of COR-Predictor-CUDA.
+//
+//   COR-Predictor-CUDA is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   COR-Predictor is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with COR-Predictor.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "COR-predictor.hpp"
+#include "COR-optimization.hpp"
+#include "COR-anneal.cpp"
+#include "COR-genetic.cpp"
+#include "COR-predictor.cpp"
+int main()
+{
+	srand((unsigned int)time(NULL));
+	COR_predictor::Get_settings();
+	COR_predictor::Get_independent();
+	COR_predictor::Get_dependent();
+	COR_predictor::Get_parameters();
+	std::cout << "Welcome to COR Predictor CUDA 0.1-alpha\n";
+	std::cout << "Copyright 2019, J. Ball (SchroedingersFerret)\n\n";
+	while (!COR_predictor::quit_cor)
+		COR_predictor::Main_menu();
+	return 0;
+}
