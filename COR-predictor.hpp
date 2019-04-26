ce4dc0f5296b64f4433db43b6963c6891f0fa73b
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-04-26 14:03:35 -0500

    Update COR-predictor.hpp

--- COR-predictor.hpp
+++ COR-predictor.hpp
@@ -35,9 +35,6 @@
 #include <cuda.h>
 #include <curand.h>
 #include <curand_kernel.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/copy.h>
 
 class COR_predictor
 {
@@ -114,45 +111,5 @@ std::vector<float> COR_predictor::dependent;
 bool COR_predictor::random_parameters = false;
 bool COR_predictor::quit_cor = false;
 std::vector<std::vector<float> > COR_predictor::parameters_global(4,std::vector<float> (6));
-
-/*Thanks to Ashwin on Code Yarns for this method: https://codeyarns.com/2011/04/09/how-to-pass-thrust-device-vector-to-kernel/
-
-The Kernel Array structure is a way to convert higher dimensional arrays into 1-d arrays that the kernel can understand
-The three size variables keep track of the dimensions of the original array*/
-
-template<typename T>
-struct KernelArray
-{
-	T* array;
-	size_t size_i;
-	size_t size_j;
-	size_t size_k;
-	
-	KernelArray(thrust::device_vector<T>& dVec) 
-	{
-		array = thrust::raw_pointer_cast( &dVec[0] );
-		size_i = dVec.size();
-		size_j = 1;
-		size_k = 1;
-  	}
-	
-	KernelArray(thrust::host_vector<T>& dVec) 
-	{
-		array = thrust::raw_pointer_cast( &dVec[0] );
-		size_i = dVec.size();
-		size_j = 1;
-		size_k = 1;
-	}
-	KernelArray(){};
-};
-
-template<typename T>
-struct HostArray
-{
-	thrust::host_vector<T> array;
-	size_t size_i;
-	size_t size_j;
-	size_t size_k;
-};
 
 #endif /* COR_PREDICTOR_HPP_ */
