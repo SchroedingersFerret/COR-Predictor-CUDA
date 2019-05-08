772f15d773265878ba37d9dc79972a64395c9288
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-05-08 11:22:38 -0500

    Update k-hArray.h

--- k-hArray.h
+++ k-hArray.h
@@ -68,21 +68,66 @@ struct HostArray
 };
 
 template <typename T>
-KernelArray<T> convertToKernel(thrust::device_vector<T> &dVec, int i, int j, int k);
+KernelArray<T> convertToKernel(thrust::device_vector<T> &dVec, int i, int j, int k)
+{
+	KernelArray<T> kArray;
+	kArray.array = thrust::raw_pointer_cast(&dVec[0]);
+	kArray.size_i = i;
+	kArray.size_j = j;
+	kArray.size_k = k;
+	return kArray;
+}
 
 template <typename T>
-KernelArray<T> convertToKernel(thrust::host_vector<T> &hVec, int i, int j, int k);
+KernelArray<T> convertToKernel(thrust::host_vector<T> &hVec, int i, int j, int k)
+{
+	KernelArray<T> kArray;
+	kArray.array = thrust::raw_pointer_cast(&hVec[0]);
+	kArray.size_i = i;
+	kArray.size_j = j;
+	kArray.size_k = k;
+	return kArray;
+}
 
 template <typename T>
-HostArray<T> convertToHost(thrust::host_vector<T> &hVec, int i, int j, int k);
+HostArray<T> convertToHost(thrust::host_vector<T> &hVec, int i, int j, int k)
+{
+	HostArray<T> hArray;
+	hArray.array = hVec;
+	hArray.size_i = i;
+	hArray.size_j = j;
+	hArray.size_k = k;
+	return hArray;
+}
 
 template <typename T>
-void flatten2dToDevice(thrust::device_vector<T> &device, std::vector<std::vector<T> > &v);
+void flatten2dToDevice(thrust::device_vector<T> &device, std::vector<std::vector<T> > &v)
+{
+	int ni = v.size();
+	int nj = v[0].size();
+	for (int i=0; i<ni; ++i)
+		for (int j=0; j<nj; ++j)
+			device[j+nj*i] = v[i][j];
+}
 
 template <typename T>
-void flatten2dToHost(thrust::host_vector<T> &host, std::vector<std::vector<T> > &v);
+void flatten2dToHost(thrust::host_vector<T> &host, std::vector<std::vector<T> > &v)
+{
+	int ni = v.size();
+	int nj = v[0].size();
+	for (int i=0; i<ni; ++i)
+		for (int j=0; j<nj; ++j)
+			host[j+nj*i] = v[i][j];
+}
 
 template <typename T>
-std::vector<std::vector<T> > recover2d(thrust::host_vector<T> &host, int ni, int nj);
+std::vector<std::vector<T> > recover2d(thrust::host_vector<T> &host, int ni, int nj)
+{
+	std::vector<std::vector<T> > result(ni,std::vector<T>(nj));
+	for (int i=0; i<ni; ++i)
+		for (int j=0; j<nj; ++j)
+			result[i][j] = host[j+nj*i];
+	return result;
+}
 
 #endif /* KH_ARRAY_H_ */
