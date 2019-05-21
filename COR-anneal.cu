3f3b76dff9678d9dda6969cedc9e803ef1711e9c
SchroedingersFerret <36749972+SchroedingersFerret@users.noreply.github.com>
2019-05-21 12:31:10 -0500

    Update and rename COR-anneal.cpp to COR-anneal.cu

--- COR-anneal.cu
+++ COR-anneal.cu
@@ -23,9 +23,6 @@
 #include <cmath>
 #include <float.h>
 #include <stdlib.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/copy.h>
 #include "k-hArray.h"
 #include "COR-anneal.h"
 
